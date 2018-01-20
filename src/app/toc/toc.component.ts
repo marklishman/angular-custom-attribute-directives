b73c5d4e9b22d82eeb58a801665276b35bc0987a
marklishman <mark.lishman@gmail.com>
2018-01-20 10:13:11 +0000

    Table of Contents

---
+++ src/app/toc/toc.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc',
+  template: `
+    <div appToc>
+      <h1>first heading 1</h1>
+      <h2>first heading 2</h2>
+      <p>para one</p>
+      <h2>second heading 2</h2>
+      <p>para two</p>
+      <h3>first heading 3</h3>
+      <p>para three</p>
+      <h4>first heading 4</h4>
+      <p>para four</p>
+      <h5>first heading 5</h5>
+      <p>para five</p>
+      <h5>second heading 5</h5>
+      <p>para six</p>
+      <h6>first heading 6</h6>
+      <p>para seven</p>
+      <h2>third heading 2</h2>
+      <p>para eight</p>
+      <h3>second heading 3</h3>
+      <p>para nine</p>
+      <h4>second heading 4</h4>
+      <p>para ten</p>
+    </div>
+    <footer>Hover your mouse over the text above to see the TOC</footer>
+  `,
+  styles: [`
+    h1, h2, h3, h4, h5, h6, p {
+      margin: 0;
+      padding: 2px;
+    }
+    footer {
+      color: green;
+      font-size: 20px;
+      margin-top: 0;
+    }
+  `]
+})
+export class TocComponent {
+}
---
+++ src/app/toc/toc.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appToc]'
+})
+export class TocDirective implements OnInit{
+
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    let toc = 'Table of Contents';
+    const headingElements = this.elementRef.nativeElement.querySelectorAll('h1,h2,h3,h4,h5,h6');
+    for (let i = 0; i < headingElements.length; i++) {
+      const heading = headingElements.item(i);
+      const level = heading.tagName[1];
+      toc += '\n' + ' '.repeat(level) + heading.textContent;
+    }
+    this.renderer.setProperty(this.elementRef.nativeElement, 'title', toc);
+  }
+
+}
