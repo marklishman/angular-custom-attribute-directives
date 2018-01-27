96ad2e342a245a1880b0cbe687ac548447d789e5
marklishman <mark.lishman@gmail.com>
2018-01-27 08:58:15 +0000

    Toggle

--- src/app/toggle/toggle-para.component.ts
+++ src/app/toggle/toggle-para.component.ts
@@ -3,20 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <header>Click on a paragraph to toggle selection</header>
-    <p appSelected>
-      Angular turns your templates into code that's highly optimized for today's JavaScript virtual machines,
-      giving you all the benefits of hand-written code with the productivity of a framework.
-    </p>
-
-    <p appSelected>
-      Serve the first view of your application on node.js, .NET, PHP, and other servers for near-instant
-      rendering in just HTML and CSS. Also paves the way for sites that optimize for SEO.
-    </p>
-
-    <p appSelected>
-      Angular apps load quickly with the new Component Router, which delivers automatic code-splitting
-      so users only load code required to render the view they request.
+    <header>Click on paragraphs to toggle selection</header>
+    <p appToggle *ngFor="let para of paras">
+      {{para}}
     </p>
   `,
   styles: [`
@@ -32,4 +21,12 @@ import { Component } from '@angular/core';
   `]
 })
 export class ToggleParaComponent {
+  paras: string[] = [
+    'Angular turns your templates into code that\'s highly optimized for today\'s JavaScript virtual machines, ' +
+    'giving you all the benefits of hand-written code with the productivity of a framework.',
+    'Serve the first view of your application on node.js, .NET, PHP, and other servers for near-instant ' +
+    'rendering in just HTML and CSS. Also paves the way for sites that optimize for SEO.',
+    'Angular apps load quickly with the new Component Router, which delivers automatic code-splitting ' +
+    'so users only load code required to render the view they request.'
+    ];
 }
