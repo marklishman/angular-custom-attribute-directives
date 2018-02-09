cc3b50dc5069f5a22872e419a68066ce8bd49753
marklishman <mark.lishman@gmail.com>
2018-02-09 20:31:24 +0000

    Tidied text snippets

--- src/app/text-snippets/text-snippets.component.ts
+++ src/app/text-snippets/text-snippets.component.ts
@@ -32,14 +32,16 @@ import { Component } from '@angular/core';
 })
 export class TextSnippetComponent {
   movieQuote = `
-     Didn’t see the first shark for about a half-hour. Tiger. 13-footer. You know how you know that
-     in the water, Chief? You can tell by lookin’ from the dorsal to the tail. What we didn’t know,
-     was that our bomb mission was so secret, no distress signal had been sent. They didn’t even list
-     us overdue for a week. Very first light, Chief, sharks come cruisin’ by, so we formed ourselves
-     into tight groups. It was sorta like you see in the calendars, you know the infantry squares in
-      the old calendars like the Battle of Waterloo and the idea was the shark come to the nearest man,
-       that man he starts poundin’ and hollerin’ and sometimes that shark he go away… but sometimes
-       he wouldn’t go away.`;
+      Didn’t see the first shark for about a half-hour. Tiger. 13-footer.
+      You know how you know that in the water, Chief? You can tell by lookin’
+      from the dorsal to the tail. What we didn’t know, was that our bomb
+      mission was so secret, no distress signal had been sent. They didn’t
+      even list us overdue for a week. Very first light, Chief, sharks come
+      cruisin’ by, so we formed ourselves into tight groups. It was sorta
+      like you see in the calendars, you know the infantry squares in the
+      old calendars like the Battle of Waterloo and the idea was the shark
+      come to the nearest man, that man he starts poundin’ and hollerin’
+      and sometimes that shark he go away… but sometimes he wouldn’t go away.`;
 
   text: string;
 
