b7d9e5aa6c3e826869dd6087c2a59166b76cbb2d
marklishman <mark.lishman@gmail.com>
2018-02-09 20:27:09 +0000

    Tidied text snippets

--- src/app/text-snippets/text-snippets.component.ts
+++ src/app/text-snippets/text-snippets.component.ts
@@ -7,12 +7,7 @@ import { Component } from '@angular/core';
       <header [hidden]="speech.snippets.length">Select some of the text below</header>
       <header [hidden]="!speech.snippets.length">Select some more to keep adding to the list</header>
       <p appTextSelector #speech="appTextSelector" (selected)="onSelected($event)">
-        Didn’t see the first shark for about a half-hour. Tiger. 13-footer. You know how you know that in the water, Chief?
-        You can tell by lookin’ from the dorsal to the tail. What we didn’t know, was that our bomb mission was so secret,
-        no distress signal had been sent. They didn’t even list us overdue for a week. Very first light, Chief, sharks come
-        cruisin’ by, so we formed ourselves into tight groups. It was sorta like you see in the calendars, you know the infantry
-        squares in the old calendars like the Battle of Waterloo and the idea was the shark come to the nearest man, that man
-        he starts poundin’ and hollerin’ and sometimes that shark he go away… but sometimes he wouldn’t go away.
+        {{movieQuote}}
       </p>
       <p>Last selection: <em>{{text || 'none'}}</em></p>
       <a [hidden]="!speech.snippets.length"
@@ -27,7 +22,7 @@ import { Component } from '@angular/core';
   styles: [`
     div, p {
       margin: 0 10px;
-      max-width: 550px;
+      max-width: 650px;
     }
     em {
       color: FireBrick;
@@ -36,10 +31,19 @@ import { Component } from '@angular/core';
   `]
 })
 export class TextSnippetComponent {
+  movieQuote = `
+     Didn’t see the first shark for about a half-hour. Tiger. 13-footer. You know how you know that
+     in the water, Chief? You can tell by lookin’ from the dorsal to the tail. What we didn’t know,
+     was that our bomb mission was so secret, no distress signal had been sent. They didn’t even list
+     us overdue for a week. Very first light, Chief, sharks come cruisin’ by, so we formed ourselves
+     into tight groups. It was sorta like you see in the calendars, you know the infantry squares in
+      the old calendars like the Battle of Waterloo and the idea was the shark come to the nearest man,
+       that man he starts poundin’ and hollerin’ and sometimes that shark he go away… but sometimes
+       he wouldn’t go away.`;
+
   text: string;
 
   onSelected(text: string) {
     this.text = text;
   }
 }
-
