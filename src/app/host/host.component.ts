14468ec609f13a83990172522534d224788dfbde
marklishman <mark.lishman@gmail.com>
2018-02-17 10:57:14 +0000

    Reducing code size

--- src/app/host/host.component.ts
+++ src/app/host/host.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-host',
   template: `
-    <header [hidden]="show">Click in the box to change hover color, double click to remove.</header>
-    <header [hidden]="!show">Enter an input field to see the border, press 'esc' key to remove.</header>
+    <header [hidden]="show">
+      Click the box to change hover color, double click to remove.
+    </header>
+    <header [hidden]="!show">
+      Enter an input field to see the border, press 'esc' key to remove.
+    </header>
     <main>
       <section class="container border">
         <input type="date">
@@ -12,9 +16,7 @@ import { Component } from '@angular/core';
       </section>
       <section>
         <p>
-          <button id="show" (click)="show = !show">
-            {{show ? 'Hide' : 'Show'}}
-          </button>
+          <button id="show" (click)="show = !show">{{show ? 'Hide' : 'Show'}}</button>
         </p>
         <header>Click the button</header>
       </section>
